179f022ba1b4a00baf2e955ec76c0fdb67a414ac
Voytsekhovskiy Vladimir <vvo@corp.muk.ua>
2016-11-21 16:05:13 +0200

    ready

--- TestTask/js/search.js
+++ TestTask/js/search.js
@@ -1,6 +1,7 @@
 "use strict";
 
 window.onload = function () {
+
     let url = "https://api.themoviedb.org/3/search/movie";
     let apiKey = "b981ca80c900d4255d6891ea75328986";
     let searchField = document.getElementById("searchField");
@@ -25,7 +26,9 @@ window.onload = function () {
             }
         ).then(function (responce) {
             requestObject = responce;
+            //noinspection JSUnresolvedVariable
             let rowArray = requestObject.results.map(function (object) {
+                //noinspection JSUnresolvedVariable
                 return TableRowFactory.createTableRow(object.id, object.title, object.original_language, object.popularity, object.vote_count, object.vote_average, object.release_date);
 
             });
@@ -83,15 +86,46 @@ window.onload = function () {
         }
 
         function processSorting() {
-            this.textContent = this.innerHTML + downArrow;
+
             let tableRowObjectArray = tableToObjectArray(tbl);
-            let sortedTableRowObjectArray = sortArrayByColumn(tableRowObjectArray, this.cellIndex);
+
+            if (this.textContent.match(downArrow)) {
+                clearHeaderArrows();
+                this.textContent = this.textContent + upArrow;
+                tbl = document.getElementById("filmsTable");
+                tableRowObjectArray = tableToObjectArray(tbl);
+                let sortedTableRowObjectArray = sortArrayByColumnDownUp(tableRowObjectArray, this.cellIndex);
+                printArray(sortedTableRowObjectArray);
+            } else if (this.textContent.match(upArrow)) {
+                clearHeaderArrows();
+                this.textContent = this.textContent + downArrow;
+                tbl = document.getElementById("filmsTable");
+                tableRowObjectArray = tableToObjectArray(tbl);
+                let sortedTableRowObjectArray = sortArrayByColumnUpDown(tableRowObjectArray, this.cellIndex);
+                printArray(sortedTableRowObjectArray);
+            } else {
+                clearHeaderArrows();
+                this.textContent = this.innerHTML + downArrow;
+                let sortedTableRowObjectArray = sortArrayByColumnUpDown(tableRowObjectArray, this.cellIndex);
+                printArray(sortedTableRowObjectArray);
+
+            }
+
+
+        }
+
+        function printArray(sortedTableRowObjectArray) {
             clearTable(tbl);
             let tableBody = document.querySelector("#filmsTable > tbody");
             sortedTableRowObjectArray.map(function (row) {
                 injectRow(tableBody, row);
             });
+        }
 
+        function clearHeaderArrows() {
+            for (let i = 0; i < header.length; i++) {
+                header[i].textContent = header[i].textContent.replace(downArrow, "").replace(upArrow, "");
+            }
         }
     }
 
@@ -99,25 +133,60 @@ window.onload = function () {
     function tableToObjectArray(table) {
         let rowArray = [];
         let tbodyContent = table.getElementsByTagName("tbody")[0].getElementsByTagName("tr");
-        for(let i=0;i<tbodyContent.length;i++){
+        for (let i = 0; i < tbodyContent.length; i++) {
             let tr = tbodyContent[i].getElementsByTagName("td");
-            rowArray.push(TableRowFactory.createTableRow(tr[0].textContent,tr[1].textContent,tr[2].textContent,tr[3].textContent,tr[4].textContent,tr[5].textContent,tr[6].textContent));
+            rowArray.push(TableRowFactory.createTableRow(tr[0].textContent, tr[1].textContent, tr[2].textContent, tr[3].textContent, tr[4].textContent, tr[5].textContent, tr[6].textContent));
         }
 
         return rowArray;
     }
 
-    function sortArrayByColumn(array, index) {
+    function sortArrayByColumnUpDown(array, index) {
 
-        return(array.sort(compare));
-
-
+        return (array.sort(compare));
 
         function compare(row1, row2) {
-            return row1.id-row2.id;
+            let keyArray = Object.keys(array[0]);
+
+            if (!isNaN(Number(row1[keyArray[index]]))) {
+                return row1[keyArray[index]] - row2[keyArray[index]];
+            } else if (row1[keyArray[index]].match(/\d{4}-\d{2}-\d{2}/) !== null) {
+                return (new Date(row1[keyArray[index]])) - (new Date(row2[keyArray[index]]));
+            } else {
+                if (row1[keyArray[index]].replace('\t', '') > row2[keyArray[index]].replace('\t', '')) {
+                    return 1;
+                }
+                else if (row1[keyArray[index]].replace('\t', '') < row2[keyArray[index]].replace('\t', '')) {
+                    return -1;
+                }
+                return 0;
+            }
+
         }
     }
 
+    function sortArrayByColumnDownUp(array, index) {
+
+        return (array.sort(compare));
+
+        function compare(row1, row2) {
+            let keyArray = Object.keys(array[0]);
+
+            if (!isNaN(Number(row1[keyArray[index]]))) {
+                return row2[keyArray[index]] - row1[keyArray[index]];
+            } else if (row1[keyArray[index]].match(/\d{4}-\d{2}-\d{2}/) !== null) {
+                return (new Date(row2[keyArray[index]])) - (new Date(row1[keyArray[index]]));
+            } else {
+                if (row1[keyArray[index]].replace('\t', '') < row2[keyArray[index]].replace('\t', '')) {
+                    return 1;
+                }
+                else if (row1[keyArray[index]].replace('\t', '') > row2[keyArray[index]].replace('\t', '')) {
+                    return -1;
+                }
+                return 0;
+            }
+        }
+    }
 
 
     function clearTable(table) {
@@ -128,14 +197,6 @@ window.onload = function () {
 
 
 };
-
-
-
-/*
-
-}*/
-
-
 
 
 
